ad5efe4feaef06efc92987a70c09594bfd1ebd66
shoiraTa <shoira.shakirovna@gmail.com>
2022-09-18 17:20:48 +0300

    feat: mirroring row

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,7 @@ defmodule Iden do
     input
     |> to_hash
     |> pick_color
+    |> build_grid
   end
 
   def to_hash(input) do
@@ -10,13 +11,23 @@ defmodule Iden do
     |> :binary.bin_to_list
     %Iden.Image{hex: hex}
   end
+
   def pick_color(image) do
     %Iden.Image{hex: hex_list} = image
     # %Iden.Image{hex:  [r, g, b | _tail] } = image
     [r, g, b | _tail] = hex_list
     %Iden.Image{image | color: {r,g,b}}
-
-
-
   end
+
+  def build_grid(%Iden.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    |> mirror_row
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
 end
